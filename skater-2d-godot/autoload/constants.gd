cee6f851f3ac05ff757a9fedb7cc7d02e6697b1a
3M Arya <131737618+SLJ900-BRIDGE-GIRDER@users.noreply.github.com>
2025-10-18 15:11:43 -0500

    feature:procedural stage generation

---
+++ skater-2d-godot/autoload/constants.gd
@@ -0,0 +1,6 @@
+extends Node
+
+const STAGE_PIECES: Dictionary[String, String] = {
+	"small_v":"res://stage_pieces/small_slope.tscn",
+	"ramp_down_heavy": "res://stage_pieces/ramp_down_heavy.tscn"
+}
--- skater-2d-godot/world_2d.gd
+++ skater-2d-godot/world_2d.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
-var slope = preload("res://stage_pieces/small_slope.tscn")
-var current_piece_end_pos = Vector2(0, 0)
+var slope: StaticBody2D
+var previous_piece_end_pos = Vector2(72, 12)
+@onready var stage_marker: Area2D = $StageMarker
+
 
 func _on_area_entered(area: Area2D) -> void:
 	print("ENTERED")
-	var instance = slope.instantiate()
-	instance.position = get_global_mouse_position()
-	add_child.call_deferred(instance)
+	var current_instance = ResourceLoader.load(Constants.STAGE_PIECES.values().pick_random()).instantiate()
+	
+	current_instance.position = previous_piece_end_pos
+	previous_piece_end_pos = current_instance.get_node("EndMarker").global_position
+	stage_marker.position = current_instance.get_node("EndMarker").global_position
+	
+	
+	add_child.call_deferred(current_instance)
