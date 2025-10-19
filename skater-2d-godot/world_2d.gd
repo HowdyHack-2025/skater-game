8150d67daedaf2f98377d6aaed9e0a9bafecf8db
3M Arya <131737618+SLJ900-BRIDGE-GIRDER@users.noreply.github.com>
2025-10-18 19:50:54 -0500

    feature:angled tiles with collisions

--- skater-2d-godot/world_2d.gd
+++ skater-2d-godot/world_2d.gd
@@ -1,17 +1,17 @@
 extends Node2D
 
+@onready var beginning: Node2D = $Beginning
+
 var slope: StaticBody2D
-var previous_piece_end_pos = Vector2(72, 12)
+@onready var previous_piece_end_pos = beginning.get_node("EndMarker").global_position
 @onready var stage_marker: Area2D = $StageMarker
 
 
 func _on_area_entered(area: Area2D) -> void:
-	print("ENTERED")
 	var current_instance = ResourceLoader.load(Constants.STAGE_PIECES.values().pick_random()).instantiate()
 	
 	current_instance.position = previous_piece_end_pos
 	previous_piece_end_pos = current_instance.get_node("EndMarker").global_position
 	stage_marker.position = current_instance.get_node("EndMarker").global_position
-	
 	
 	add_child.call_deferred(current_instance)
