8e3c2b32f49071d91f3a419f4d2faebd484fbde9
Nianjin Yang <nianjindev@gmail.com>
2025-10-18 19:51:02 -0500

    Merge remote-tracking branch 'refs/remotes/origin/main' merged

---
+++ skater-2d-godot/status.gd
@@ -0,0 +1,15 @@
+extends Panel
+
+var current_status: String = ""
+@onready var label: Label = $Label
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	SignalBus.status_update.connect(update_status)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	label.text = current_status
+	
+func update_status(status: String):
+	current_status = status
