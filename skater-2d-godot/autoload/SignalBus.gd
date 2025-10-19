66d5e2ec3f48eb56e1157f720c6de89364fc74a1
Nianjin Yang <nianjindev@gmail.com>
2025-10-19 11:48:28 -0500

    broken menu

--- skater-2d-godot/autoload/SignalBus.gd
+++ skater-2d-godot/autoload/SignalBus.gd
@@ -5,12 +5,17 @@ signal status_update2(status: String)
 signal p1win
 signal p2win
 var winned = false
-
+var in_s = false
 var camera_position: Vector2
-@onready var progress: ProgressBar = get_tree().current_scene.find_child("ProgressBar")
-@onready var camera: Camera2D = get_tree().current_scene.find_child("Camera2D")
+@onready var progress: ProgressBar
+@onready var camera: Camera2D
 
 func _process(delta: float) -> void:
-	camera_position = camera.global_position
-	progress.max_value = 10000
-	progress.value = camera.position.x
+	if get_tree().current_scene.name == "World2D":
+		in_s = true
+		camera = get_tree().current_scene.find_child("Camera2D")
+		progress = get_tree().current_scene.find_child("ProgressBar")
+	if in_s:
+		camera_position = camera.global_position
+		progress.max_value = 10000
+		progress.value = camera.position.x
