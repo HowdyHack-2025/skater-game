803293731a3e424139a7a2c6d05b8e53ec7324a6
3M Arya <131737618+SLJ900-BRIDGE-GIRDER@users.noreply.github.com>
2025-10-19 11:48:16 -0500

    polish

--- skater-2d-godot/autoload/constants.gd
+++ skater-2d-godot/autoload/constants.gd
@@ -11,5 +11,7 @@ const STAGE_PIECES: Dictionary[String, String] = {
 	"U45":"res://stage_pieces/U/u_45.tscn",
 	"H1": "res://stage_pieces/H/h_1.tscn",
 	"H2":"res://stage_pieces/H/h_2.tscn",
+	"H3":"res://stage_pieces/U/u_3.tscn",
+	"H4":"res://stage_pieces/U/u_4.tscn",
 	"H45":"res://stage_pieces/H/h_45.tscn"
 }
