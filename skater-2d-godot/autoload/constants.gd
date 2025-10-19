b77e724da4896dc291191ebdf02574637c3bc207
3M Arya <131737618+SLJ900-BRIDGE-GIRDER@users.noreply.github.com>
2025-10-19 00:33:35 -0500

    parallax

--- skater-2d-godot/autoload/constants.gd
+++ skater-2d-godot/autoload/constants.gd
@@ -3,5 +3,7 @@ extends Node
 const STAGE_PIECES: Dictionary[String, String] = {
 	"small_v":"res://stage_pieces/small_slope.tscn",
 	"ramp_down_heavy": "res://stage_pieces/ramp_down_heavy.tscn",
-	"U45":"res://stage_pieces/U/u_45.tscn"
+	"U45":"res://stage_pieces/U/u_45.tscn",
+	"H1": "res://stage_pieces/H/h_1.tscn",
+	"H2":"res://stage_pieces/H/h_2.tscn"
 }
